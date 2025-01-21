ae505d436612f737b823415dd776d0f208d251c5
Vinicius Victor <116410122+vinevictor@users.noreply.github.com>
2025-01-21 16:50:03 -0300

    :bug: fix

--- .next/static/chunks/src_app_layout_tsx_61af54._.js
+++ .next/static/chunks/src_app_layout_tsx_61af54._.js
@@ -6,7 +6,13 @@
   "path": "static/chunks/src_app_layout_tsx_61af54._.js",
   "chunks": [
     "static/chunks/[next]_internal_font_google_55dcbe._.css",
-    "static/chunks/_7cb389._.js"
+    "static/chunks/node_modules_next_dist_6b829c._.js",
+    "static/chunks/node_modules_@chakra-ui_react_dist_esm_9a795b._.js",
+    "static/chunks/node_modules_@emotion_2a9a5a._.js",
+    "static/chunks/node_modules_@zag-js_0cb174._.js",
+    "static/chunks/node_modules_@floating-ui_7349b7._.js",
+    "static/chunks/node_modules_752d0f._.js",
+    "static/chunks/src_aa16ce._.js"
   ],
   "source": "dynamic"
 });
