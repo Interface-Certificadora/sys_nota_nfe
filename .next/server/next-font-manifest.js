52cd625b3fbdc6d0f08b705697880029296dff51
Vinicius Victor <116410122+vinevictor@users.noreply.github.com>
2025-01-23 09:35:09 -0300

    fix

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/ti002/Documentos/GitHub/sys_nota_nfe/src/app/layout\":[\"static/media/0f3a385557f1712f-s.p.woff2\",\"static/media/47cbc4e2adbc5db9-s.p.woff2\",\"static/media/cd0918d767a3fa86-s.p.woff2\",\"static/media/92f44bb82993d879-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
