cf61d9302c2963713abcf00dce1bfa8a62dc6284
Vinicius Victor <116410122+vinevictor@users.noreply.github.com>
2025-01-24 08:52:22 -0300

    🧹 cleanup: comentarios

--- src/app/(public_route)/(support)/faqvideos/page.tsx
+++ src/app/(public_route)/(support)/faqvideos/page.tsx
@@ -70,7 +70,6 @@ export default function Page() {
   const handleScroll = () => {
     const container = scrollContainerRef.current;
     if (container) {
-      // Verifica se o scroll atingiu o final
       const isBottom = container.scrollHeight - container.scrollTop <= container.clientHeight + 1;
       setIsAtBottom(isBottom);
     }
@@ -115,98 +114,3 @@ export default function Page() {
       </Flex>
   );
 }
-
-
-
-// import ContatoSup from "@/components/cards/contato_sup";
-// import CardVideos from "@/components/cards/videos_card";
-// import { Flex } from "@chakra-ui/react";
-
-// export default function PageVideos() {
-
-//     const testeVideos = [{ 
-//         "src" :  "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" ,
-//         "title" : "Big Buck Bunny"
-//       },
-//       { 
-//         "src" :  "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4" ,
-//         "title" : "Elephant Dream"
-//       },
-//       { 
-//         "src" :  "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4" ,
-//         "title" : "For Bigger Blazes"
-//       },
-//       { 
-//         "src" : "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4" ,
-//         "title" : "For Bigger Escape"
-//       },
-//       { 
-//         "src" : "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4" ,
-//         "title" : "For Bigger Fun"
-//       },
-//       { 
-//         "src" : "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4" ,
-//         "title" : "For Bigger Joyrides"
-//       },
-//       { 
-//         "src" : "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4" ,
-//         "title" : "For Bigger Meltdowns"
-//       },
-//       { 
-//         "src" : "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4" ,
-//         "title" : "Sintel"
-//       },
-//       { 
-//         "src" : "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/SubaruOutbackOnStreetAndDirt.mp4" ,
-//         "title" : "Subaru Outback On Street And Dirt"
-//       },
-//       { 
-//         "src" : "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4" ,
-//         "title" : "Tears of Steel"
-//       },
-//       { 
-//         "src" : "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/VolkswagenGTIReview.mp4" ,
-
-//         "title" : "Volkswagen GTI Review"
-//       },
-//       { 
-//         "src" : "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4" ,
-
-//         "title" : "We Are Going On Bullrun"
-//       },
-//       { 
-//         "src" : "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WhatCarCanYouGetForAGrand.mp4" ,
-//         "title" : "What care can you get for a grand?"
-//       }
-// ]
-
-//     return (
-//         <>
-//         <Flex
-//         flexDir={'column'}
-//         w={'100%'}
-//         >
-//             <Flex
-//                 w="100%"
-//                 h={'100%'}
-//                 overflowY="auto"
-//                 flexDirection={{base: 'column', md:'row'}}
-//                 flexWrap={{base:'nowrap', md:'wrap'}}
-//                 alignItems={'center'}
-//             >
-//                 {testeVideos.map((video, index) => <CardVideos key={index} src={video.src} title={video.title} />)}
-                
-//             <ContatoSup w="40%" h="10%" />
-//             </Flex>
-
-//           {/* <Flex
-//           justifyContent={'center'}
-//           bg={'hidden'}
-//           h={'10%'}>
-//           </Flex> */}
-//         </Flex>
-
-//         </>
-//     )
-
-// }
