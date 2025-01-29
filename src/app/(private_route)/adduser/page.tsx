18dc3feeddbe60118411c1dbbf408f87a2968fe0
Vinicius Victor <116410122+vinevictor@users.noreply.github.com>
2025-01-29 10:15:24 -0300

    feat: cript session

---
+++ src/app/(private_route)/adduser/page.tsx
@@ -0,0 +1,13 @@
+import { Flex } from "@chakra-ui/react";
+
+export default function AddUser() {
+    return (
+        <>
+        <Flex w={'full'} flexDir={'column'} h={'full'} >
+
+
+            
+        </Flex>
+        </>
+    )
+}
