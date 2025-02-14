1ba66fc915365645aa223f843fcc7721282af835
Vinicius Victor <116410122+vinevictor@users.noreply.github.com>
2025-02-14 10:05:58 -0300

    relacionamento

---
+++ src/types/parceiro.type.ts
@@ -0,0 +1,12 @@
+export interface Parceiro {
+    id: number; 
+    inativos: number; 
+    nome: string; 
+    email: string; 
+    telefone: string; 
+    chave_pix: string; 
+    createdAt: string; 
+    updatedAt: string; 
+    ativos: number; 
+    whatsapp: boolean;
+}
