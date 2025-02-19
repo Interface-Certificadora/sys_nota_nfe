de63c374cf4c19f30d0db29d0a41649d4afbb1ae
Alexandre Henrique R Araujo <64115785+alexandre-henrique-rp@users.noreply.github.com>
2025-02-19 11:34:35 -0300

    update

--- src/app/api/cliente/path/[id]/route.ts
+++ src/app/api/cliente/path/[id]/route.ts
@@ -1,42 +1,44 @@
-
 import AuthService from "@/modules/auth/services/auth-service";
 import { NextResponse } from "next/server";
 
-
-export async function PATCH (request: Request, { params }: { params: { id: string } }
+export async function PATCH(
+  request: Request,
+  { params }: { params: { id: string } }
 ) {
-    try {
-        const body = await request.json();
-        
-        const { id } = params;
-        const sessionData = await AuthService.sessionUser();
-        const session = sessionData?.data;
+  try {
+    const body = await request.json();
 
-        if (!session) {
-            console.error("Usuário não autenticado. Token ausente.");
-            return NextResponse.json(
-                { error: true, message: "Usuário não autenticado." },
-                { status: 401 }
-            );
-        }
+    const { id } = params;
+    const sessionData = await AuthService.sessionUser();
+    const session = sessionData?.data;
 
-        console.log(body);
-        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/cliente/${id}`, {
-            method: "PATCH",
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${session.token}`,
-            },
-            body: JSON.stringify(body),
-        });
-        const data = await response.json();
-        return NextResponse.json(data, { status: 200 });
-
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    } catch (error) { 
-        return NextResponse.json(
-            { error: true, message: error, data: null },
-            { status: 500 }
-        );
+    if (!session) {
+      console.error("Usuário não autenticado. Token ausente.");
+      return NextResponse.json(
+        { error: true, message: "Usuário não autenticado." },
+        { status: 401 }
+      );
     }
+
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/cliente/${id}`,
+      {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${session.token}`
+        },
+        body: JSON.stringify(body)
+      }
+    );
+    const data = await response.json();
+    return NextResponse.json(data, { status: 200 });
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (error) {
+    return NextResponse.json(
+      { error: true, message: error, data: null },
+      { status: 500 }
+    );
+  }
 }
