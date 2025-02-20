701fb13493fa1c128d574cd3f7c56816a0008d0d
vitiima <matheus_famas@hotmail.com>
2025-02-20 13:08:16 -0300

    update

--- src/app/(private_route)/home/CaptureIPClient.tsx
+++ src/app/(private_route)/home/CaptureIPClient.tsx
@@ -4,17 +4,13 @@ import { useEffect } from "react";
 export default function CaptureIPClient() {
 
   useEffect(() => {
-    
-
     async function captureIP() {
       try {
-       
-
         const res = await fetch("https://api.ipify.org?format=json");
         const data = await res.json();
         const userIP = data.ip;
 
-        
+        console.log("ipcapturado", userIP);
 
         await fetch("/api/ip/capture", {
           method: "POST",
@@ -22,7 +18,7 @@ export default function CaptureIPClient() {
           body: JSON.stringify({ ip: userIP }),
         });
 
-        
+
 
       } catch (error) {
         console.error("❌ Erro ao capturar o IP do usuário:", error);
