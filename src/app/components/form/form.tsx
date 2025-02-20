6388c4055479d57be37c5990bb0da96d5b7aca2b
Alexandre Henrique R Araujo <64115785+alexandre-henrique-rp@users.noreply.github.com>
2025-02-20 15:18:23 -0300

    correção

--- src/app/components/form/form.tsx
+++ src/app/components/form/form.tsx
@@ -2,7 +2,8 @@
 "use client";
 import { toaster } from "@/app/components/ui/toaster";
 import { LoadingContext } from "@/context/LoadingContext";
-import { PropsWithChildren, useContext, useEffect} from "react";
+import { useRouter } from "next/navigation";
+import { PropsWithChildren, useContext, useEffect } from "react";
 import { useFormState } from "react-dom";
 
 type HTMLFormProps = React.DetailedHTMLProps<
@@ -13,44 +14,46 @@ type HTMLFormProps = React.DetailedHTMLProps<
 type FormProps = PropsWithChildren<
   Omit<HTMLFormProps, "action"> & {
     action: (prevState: any, formData: FormData) => Promise<any>;
+    url?: string;
   }
 >;
 
 export function FormComponent(props: FormProps) {
-  
-  const { setLoading } = useContext(LoadingContext)
-
+  const { setLoading } = useContext(LoadingContext);
   const [state, formAction] = useFormState(props.action, { error: null });
-
- 
+  const Router = useRouter();
 
   const handleSubmit = () => {
-    setLoading(true)
+    setLoading(true);
   };
-  
 
   useEffect(() => {
-  
-
     if (state?.message) {
       toaster.create({
         title: state.error ? "Erro" : "Sucesso",
         description: state.message,
         type: state.error ? "error" : "success",
-        duration: 3000,
+        duration: 6000,
       });
-    }
 
-    if (state?.success) {
-      setTimeout(() => {
-        window.location.reload();
-      }, 2000);
+      if (!state?.error) {
+        (async () => {
+          if (props.url) {
+            await new Promise((resolve) => setTimeout(resolve, 1000));
+            Router.push(props.url);
+          }
+        })();
+      }
+
+      if (state?.success) {
+        setTimeout(() => {
+          window.location.reload();
+        }, 1000);
+      }
     }
 
     setLoading(false);
-    
-  }, [state, setLoading]);
-
+  }, [state, setLoading, Router, props.url]);
 
   return <form {...props} action={formAction} onSubmit={handleSubmit} />;
 }
